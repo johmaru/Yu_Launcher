8820e29f63b18aa95bfcf87bb55087aeaecf6d46
Jo Matsumaru <hatunehimeka@outlook.jp>
2024-10-17 19:14:51 +0900

    hotfix

--- YuLauncher/Program.cs
+++ YuLauncher/Program.cs
@@ -2,28 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using NLog;
 using NLog.Config;
 using Velopack;
+using YuLauncher.Core.lib;
 
 namespace YuLauncher
 {
     internal class Program
     {
-        [STAThread]
         public static void Main(string[] args)
         {
-            var config = new XmlLoggingConfiguration("NLog.config");
-            LogManager.Configuration = config;
-            VelopackApp.Build().WithBeforeUninstallFastCallback((v) => {
-            }).WithFirstRun((v) => {
-                MessageBox.Show("Thanks for installing my application!");
-            }).Run();
-            var application = new App();
-            application.InitializeComponent();
-            application.Run();
+            try
+            {
+                var config = new XmlLoggingConfiguration("NLog.config");
+                LogManager.Configuration = config;
+                VelopackApp.Build().WithBeforeUninstallFastCallback((v) => { }).WithFirstRun((v) =>
+                {
+                    MessageBox.Show("Thanks for installing my application!");
+                }).Run();
+                
+                
+               Thread thread = new Thread(() =>
+                {
+                    var app = new App();
+                    app.InitializeComponent();
+                    app.Run();
+                });
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
+                thread.Join();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                LoggerController.LogError($"{e}");
+                throw;
+            }
         }
     }
 }
