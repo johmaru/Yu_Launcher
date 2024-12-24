7a715ecf5c44c316b360b7700d9dcc460a203204
Jo Matsumaru <hatunehimeka@outlook.jp>
2024-12-24 20:04:09 +0900

    GameWindowのバグ修正  & アップデート時のデータ削除修正

--- YuLauncher/Program.cs
+++ YuLauncher/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -27,15 +28,7 @@ namespace YuLauncher
                 }).Run();
                 
                 
-               Thread thread = new Thread(() =>
-                {
-                        var app = new App();
-                        app.InitializeComponent();
-                        app.Run();
-                });
-                thread.SetApartmentState(ApartmentState.STA);
-                thread.Start();
-                thread.Join();
+             
             }
             catch (Exception e)
             {
@@ -43,6 +36,68 @@ namespace YuLauncher
                 LoggerController.LogError($"{e}");
                 throw;
             }
+        }
+
+        private static async void TempCheckStart()
+        {
+            try
+            {
+                await Temp();
+                await ThreadStart();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                LoggerController.LogError($"{e}");
+                throw;
+            }
+        }
+
+        private static ValueTask Temp()
+        {
+            string tempPath = Path.Combine("..", "Temp");
+            string fullPath = Path.GetFullPath(tempPath);
+            
+            if (Directory.Exists(fullPath))
+            {
+                string webviewData = Path.Combine(fullPath, "YuLauncher.exe.WebView2");
+                string webviewDataFullPath = Path.GetFullPath(webviewData);
+                
+                Directory.Move(webviewDataFullPath, "YuLauncher.exe.WebView2");
+                
+                string games = Path.Combine(fullPath, "Games");
+                string gamesFullPath = Path.GetFullPath(games);
+                
+                Directory.Move(gamesFullPath, "Games");
+                
+                string html = Path.Combine(fullPath, "html");
+                string htmlFullPath = Path.GetFullPath(html);
+                
+                Directory.Move(htmlFullPath, "html");
+                
+                string settings = Path.Combine(fullPath, "settings.toml");
+                string settingsFullPath = Path.GetFullPath(settings);
+                
+                File.Move(settingsFullPath, "settings.toml");
+                
+                Directory.Delete(fullPath);
+            }
+            return ValueTask.CompletedTask;
+        }
+        
+        private static ValueTask ThreadStart()
+        {
+            Thread thread = new Thread(() =>
+            {
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+
+            return ValueTask.CompletedTask;
         }
     }
 }
